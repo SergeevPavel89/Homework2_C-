4de168fcd357c666adeda4e9b876416180086c10
Sergeev <Pavol07@mail.ru>
2023-07-30 00:17:30 +0300

    Homework2

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+
+// int num1 = num /10%10;
+// Console.WriteLine(num1);
+
+// Задача 13: Напишите программу, 
+// которая выводит третью цифру
+//  заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// int Prompt(string msg)
+// {
+
+// System.Console.WriteLine($"{msg}");
+// return Convert.ToInt32(Console.ReadLine());
+// }
+// int number = Prompt("Введите число");
+
+// int fnumber(int number)
+// {
+// while (number > 999)
+// {
+// number /= 10;
+// }
+// return number % 10;
+// }
+
+// bool check(int number)
+// {
+// if (number < 100)
+// return false;
+// else return true;
+// }
+
+// if (check(number) != true)
+// System.Console.WriteLine("Третьей цифры нет");
+// else
+// System.Console.WriteLine($"Третья цифра числа {number} является {fnumber(number)}");
+
+// 
+// Задача 15: Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, 
+//  является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32 (Console.ReadLine());
+
+// if (num == 6 || num == 7)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
